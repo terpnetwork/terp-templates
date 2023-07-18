f74b712c557687c15736d599d467e1af7934bc7e
discoverdefiteam <discover.defi@gmail.com>
2023-07-18 10:12:24 -0400

    first commit

---
+++ .github/scripts/test_generate.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+set -u -o errexit -o nounset -o pipefail
+command -v shellcheck > /dev/null && shellcheck "$0"
+
+REPO_ROOT="$(realpath "$(dirname "$0")/../..")"
+TMP_DIR=$(mktemp -d "${TMPDIR:-/tmp}/XXXXXXXXX")
+
+echo "Navigating to $TMP_DIR"
+cd "$TMP_DIR"
+
+function test-template() {
+  TEMPLATE="${1}"
+  PROJECT_NAME="${TEMPLATE//\//-}"
+  echo
+  echo "#######################################"
+  echo "# Testing template $TEMPLATE"
+  echo "#######################################"
+  echo
+  (
+    GIT_BRANCH=$(git -C "$REPO_ROOT" branch --show-current)
+
+    echo "Generating project from local repository (branch $GIT_BRANCH) ..."
+    cargo generate --path "$REPO_ROOT" --name "$PROJECT_NAME" -d minimal=false "$TEMPLATE"
+
+    (
+      cd "$PROJECT_NAME"
+      echo "This is what was generated"
+      ls -lA
+
+      # Check formatting
+      echo "Checking formatting ..."
+      cargo fmt -- --check
+
+      echo "Running clippy ..."
+      cargo clippy -- -D warnings -A clippy::derive_partial_eq_without_eq
+
+      # Debug builds first to fail fast
+      echo "Running unit tests ..."
+      cargo unit-test
+
+      echo "Creating schema ..."
+      cargo schema
+
+      echo "Building wasm ..."
+      cargo wasm
+    )
+  )
+}
+
+if [[ -n "${1:-}" ]]; then
+  test-template "${1}"
+else
+  find "$REPO_ROOT" -name Cargo.toml -exec dirname {} \; | while read -r TEMPLATE; do
+    test-template "${TEMPLATE//$REPO_ROOT\//}"
+    echo
+  done
+fi
---
+++ README.md
@@ -0,0 +1,37 @@
+# Terp Templates
+
+Terp Smart Contract template projects used by the [Terp Developer CLI](https://github.com/terpnetwork/terp-cli).
+
+## List of templates
+
+- [Default](./default): a blank slate project
+- [Increment](./increment): a simple smart contract that increments and resets a counter
+
+## Creating a new project from a template
+
+### Recommended way
+
+Follow the instructions in the [Terp Setup tutorial](./TODO).
+
+### Using cargo-generate
+
+Assuming you have a recent version of rust and cargo (v1.51.0+) installed (via [rustup](https://rustup.rs/)),
+then the following should get you a new repo to start a contract:
+
+Install [cargo-generate](https://github.com/ashleygwilliams/cargo-generate) and cargo-run-script.
+If you didn't install them already, run the following commands:
+
+```sh
+cargo install cargo-generate --features vendored-openssl
+cargo install cargo-run-script
+```
+
+Now, use it to create your new contract.
+Go to the folder in which you want to place it and run:
+
+```sh
+cargo generate --git terpnetwork/terp-templates.git --name PROJECT_NAME default
+```
+
+You will now have a new folder called `PROJECT_NAME` (I hope you changed that to something else)
+containing a simple working contract and build system that you can customize.
